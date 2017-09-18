4067fb45f43d3ad2adc495ef4efc28d1d92f6c38
Darmody <eterlf41@gmail.com>
2017-09-18 18:48:22 +0800

    refactor to stateStream, remove createSource

---
+++ __tests__/createStateStream.spec.js
@@ -0,0 +1,55 @@
+import { createStateStream, RELAY, SOURCE } from '../src/stateStream'
+
+describe('createStateStream', () => {
+  it('throw if name is invalid', () => {
+    expect(() =>
+      createStateStream('source', SOURCE)
+    ).not.toThrow()
+
+    expect(() =>
+      createStateStream(1, SOURCE)
+    ).toThrow()
+
+    expect(() =>
+      createStateStream(Symbol(''), SOURCE)
+    ).toThrow()
+
+    expect(() =>
+      createStateStream('', SOURCE)
+    ).toThrow()
+
+    expect(() =>
+      createStateStream({}, SOURCE)
+    ).toThrow()
+
+    expect(() =>
+      createStateStream()
+    ).toThrow()
+
+    expect(() =>
+      createStateStream(() => {}, SOURCE)
+    ).toThrow()
+  })
+
+  it('throw if type is not one of [SOURCE, RELAY]', () => {
+    expect(() =>
+      createStateStream('source', SOURCE)
+    ).not.toThrow()
+
+    expect(() =>
+      createStateStream('stream', RELAY, createStateStream('source', SOURCE))
+    ).not.toThrow()
+
+    expect(() =>
+      createStateStream('source', '')
+    ).toThrow()
+
+    expect(() =>
+      createStateStream('source')
+    ).toThrow()
+
+    expect(() =>
+      createStateStream('source', () => {})
+    ).toThrow()
+  })
+})
--- src/StateStream.js
+++ src/StateStream.js
@@ -1,70 +1,119 @@
 // @flow
 import Rx from 'rxjs'
-import createSource from '../src/createSource'
-import type { Source } from '../src/createSource'
+import { createChangeEmitter } from 'change-emitter'
 
-export default class StateStream {
-  constructor(observer: Function, initialState: any, ...sources: Array<Source>) {
-    if (observer !== null && observer !== undefined && typeof observer !== 'function') {
-      throw new Error('Expected the observer to be a function.')
-    }
+export const SOURCE = 'SOURCE'
+export const RELAY = 'RELAY'
 
-    if (sources) {
-      sources.forEach((source) => {
-        if (typeof source !== 'object' || !source.name || !source.state$) {
-          throw new Error('Expected the source to be a Source.')
-        }
-      })
-    }
-
-    this.observer = observer
-    this.state$ = Rx.Observable.create((observer) => {
-      this.emit = (state: any) => observer.next(state)
-      if (initialState !== null && initialState !== undefined) {
-        this.emit(initialState)
-      }
-    })
-
-    this.connect(sources)
-  }
-
-  observer = null
-  state$ = Rx.Observable.of(null)
-  emit = () => {}
-
-  connect = (sources: Array<Source>) => {
-    let state$ = this.state$;
-
-    (sources || []).map((source) => {
-      state$ = Rx.Observable.combineLatest(state$, source.state$, (state, sourceState) => {
-        if (typeof state !== 'object') {
-          return {
-            $: state,
-            [source.name]:  sourceState
-          }
-        }
-
-        return ({
-          ...state,
-          [source.name]:  sourceState
-        })
-      })
-    })
-
-    this.state$ = state$
-  }
-
-  observeState = (selector: Function) => {
-    let state$ = this.state$
-
-    if (this.observer) {
-      state$ = this.observer(state$)
-    }
-
-    state$.subscribe({ next: selector })
-
-    return this
-  }
-
-  asSource = (name: string) => createSource(name, this.state$)
+type StreamType = 'SOURCE' | 'RELAY'
+export interface StateStream {
+  name: string,
+  state$: Rx.Observable,
+  type: StreamType,
 }
+
+type SourceStateStream = StateStream & { type: 'SOURCE', emit: Function }
+type RelayStateStream = StateStream & { type: 'RELAY' }
+
+const createSource = (name: string, initialState?: any): SourceStateStream => {
+  const emitter = createChangeEmitter()
+
+  let prevState = null
+
+  const state$ = Rx.Observable
+    .create((observer) => {
+      emitter.listen(updater => {
+        if (typeof updater !== 'function') {
+          observer.next(updater)
+        } else {
+          observer.next(updater(prevState))
+        }
+      })
+    })
+    .startWith(initialState)
+    .map(state => {
+      prevState = state
+      return state
+    })
+
+  return {
+    name,
+    state$,
+    type: SOURCE,
+    emit: emitter.emit,
+  }
+}
+
+const createRelay = (name: string, sources: Array<StateStream>): RelayStateStream => {
+  if (sources.length === 0) {
+    throw new Error('Expected the sources to be passed.')
+  }
+
+  const sourceNames = {}
+  sources.forEach((source) => {
+    if (typeof source !== 'object' || !source.name || !source.state$ || !source.type) {
+      throw new Error('Expected the source to be type of StateStream.')
+    }
+
+    sourceNames[source.name] = 1
+  })
+
+  if (Object.keys(sourceNames).length !== sources.length) {
+    throw new Error('Sources\' name should be unique.')
+  }
+
+  const states$ = sources.map(source => source.state$)
+  const state$ = Rx.Observable.combineLatest(...states$, (...states) => {
+    let state = {}
+
+    sources.forEach((source, index) => {
+      state[source.name] = states[index]
+    })
+
+    return state
+  })
+
+  return {
+    name,
+    state$,
+    type: RELAY,
+  }
+}
+
+export const createStateStream = (
+  name: string,
+  type: StreamType,
+  initialState: any,
+  ...sources: Array<StateStream>
+) => {
+  if (typeof name !== 'string' || !name) {
+    throw new Error('Expected the name to be a not none string.')
+  }
+
+  if (!type
+    || typeof type !== 'string'
+    || ![SOURCE, RELAY].find(streamType => streamType === type)
+  ) {
+    throw new Error('Expected the type to be one of [SOURCE, RELAY].')
+  }
+
+  let streamType = type.toUpperCase()
+
+  if (streamType === SOURCE) {
+    return createSource(name, initialState)
+  } else if (streamType === RELAY) {
+    return createRelay(name, [initialState, ...sources])
+  }
+
+  return null
+}
+
+export const createSourceStateStream = (
+  name: string,
+  initialState?: any
+) => createStateStream(name, SOURCE, initialState)
+
+export const createRelayStateStream = (
+  name: string,
+  sources: Array<StateStream>
+) => createStateStream(name, RELAY, ...sources)
