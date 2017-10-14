48e4094de3d40462bd30a222e849c31ac0e9ed5a
Darmody <eterlf41@gmail.com>
2017-10-14 20:45:08 +0800

    mv reactObserver to rxact-react

--- __tests__/rxact.spec.js
+++ __tests__/rxact.spec.js
@@ -2,7 +2,6 @@ import * as Rxact from '../src'
 describe('rxact', () => {
   it('exposes the public API', () => {
     expect(Rxact.StateStream).toBeDefined()
-    expect(Rxact.createReactObserver).toBeDefined()
     expect(Rxact.setup).toBeDefined()
     expect(Rxact.teardown).toBeDefined()
   })
